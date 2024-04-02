a99fda45e163ece81dc49efb2f4f5279302d9926
birtheec <Birthe.e.christiansen@gmail.com>
2024-04-02 13:27:34 +0200

    feat: ProfileView

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import MainLayout from "@/layouts/MainLayout.vue";
+import Input from "@/components/Input.vue";
+import button from "@/components/Button.vue";
+
+</script>
+
+<template>
+  <MainLayout>
+    <div class="profile">
+      <RouterLink to="/login">
+        <button class="logout">Logout</button>
+      </RouterLink>
+      <h1>My profile</h1>
+      <Input label="First name:" placeholder="Enter your first name here" :disabled="true"/>
+      <Input label="Last name:" placeholder="Enter your last name here" :disabled="true"/>
+      <Input label="Username:" placeholder="Enter your username here" :disabled="true"/>
+      <Input label="Email: " placeholder="Enter your email here" :disabled="true"/>
+    </div>
+  </MainLayout>
+
+</template>
+
+<style scoped>
+
+</style>
