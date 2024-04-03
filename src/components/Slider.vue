6f470c0cb0c7498a8883f0175240acf9d60da216
Lotte Christine Walla Aune <lcaune@stud.ntnu.no>
2024-04-03 11:31:26 +0200

    feat: Added Slider component

---
+++ src/components/Slider.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+const props = defineProps({
+  answer: {
+    type: String,
+    min: String,
+    max: String,
+    step: String,
+    required: true,
+  }
+})
+
+</script>
+
+<template>
+  <div class="slider">
+    <label :for="'slider-' + answer.min">{{ answer.min }}</label>
+    <input type="range" :id="'slider-' + answer.min" :min="answer.min" :max="answer.max" :step="answer.step"/>
+    <label :for="'slider-' + answer.max">{{ answer.max }}</label>
+  </div>
+</template>
+
+
+<style scoped>
+.slider {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.slider input[type=range] {
+  flex-grow: 1; /* Allow the range input to grow and fill the space */
+  margin: 0 10px; /* Add some space between the labels and the slider */
+}
+
+
+</style>
