1e3024c3dd0373a4a7503b6b00eec14d33abcb7f
lazyPanda90 <94993504+lazyPanda90@users.noreply.github.com>
2024-04-04 12:03:14 +0200

    feat: now sends feedback to backend

--- src/views/FeedbackView.vue
+++ src/views/FeedbackView.vue
@@ -1,15 +1,38 @@
 <script setup>
+import { ref, computed } from "vue";
+import axios from "axios"; // Import axios for HTTP requests
+import { useStore } from "vuex";
 import MainLayout from "@/layouts/MainLayout.vue";
 import TextArea from "@/components/TextArea.vue";
 import Button from "@/components/Button.vue";
-import { ref } from "vue";
+import mainStore  from "@/stores/mainStore.js";
 
+const store = useStore();
 const feedbackDescription = ref('');
+
+const user = computed(() => store.state.user)
+
 async function submitFeedback() {
+  const endpoint = 'http://localhost:8080/feedback';
+
   try {
-    console.log(feedbackDescription);
+    const payload = {
+      comment: feedbackDescription.value
+    };
+    const token = store.state.user.token;
+    const headers = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
+
+    const response = await axios.post(endpoint, payload, headers);
+    console.log(response.data)
+    feedbackDescription.value = '';
   } catch (error) {
+    // Log the error to the console
     console.error(error);
+    // Handle the error (e.g., show an error message to the user)
   }
 }
 </script>
@@ -19,8 +42,8 @@ async function submitFeedback() {
     <div class="feedback">
       <h1>Feedback</h1>
       <p>Help us improve by providing your feedback below.</p>
-      <TextArea v-model:modelValue="feedbackDescription" placeholder="Enter your feedback here..." :charLimit="200" required/>
-      <form @submit.prevent="submitFeedback">
+      <form @submit.prevent="submitFeedback" class="feedback-form">
+        <TextArea v-model:modelValue="feedbackDescription" placeholder="Enter your feedback here..." :charLimit="200" required/>
         <Button type="submit">Submit</Button>
       </form>
     </div>
