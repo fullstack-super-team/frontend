44187d99247d9cfd13e743c703fbcf29ce864420
Lotte Christine Walla Aune <lcaune@stud.ntnu.no>
2024-04-05 10:30:52 +0200

    feat: Added icon for correct and incorrect answer in AnswerButton

--- src/components/AnswerButton.vue
+++ src/components/AnswerButton.vue
@@ -1,25 +1,42 @@
 <script setup>
-import { defineProps } from 'vue';
 
 defineProps({
-  answer: {
-    type: Object,
+  answerText: {
+    type: String,
     required: true
-  }
+  },
+  isCorrect: Boolean,
+  isAnswerSelected: Boolean
 })
 
 </script>
 
 <template>
   <button class="answer-card">
-    {{ answer.text }}
+    {{ answerText }}
+    <img class="icon" v-if="isCorrect && isAnswerSelected" src="@/assets/correct.png" alt="Correct Answer" />
+    <img class="icon" v-if="!isCorrect && isAnswerSelected" src="@/assets/wrong.png" alt="Incorrect Answer" />
   </button>
 </template>
 
 <style scoped>
 .answer-card {
   padding: 20px;
-  text-align: center; /* Centers the content inside each card */
+  text-align: center;
+  color: black;
+  display: grid;
+  grid-template-columns: auto 1fr; /* First column auto-sized to content, second column takes the rest */
+  align-items: center; /* Aligns items vertically in the center */
+  gap: 10px; /* Adds some space between the text and the icon */
 }
 
+.answer-card:disabled {
+  cursor: default;
+}
+
+.icon {
+  width: 30px; /* Adjusts the width of the icon */
+  height: auto; /* Keeps the aspect ratio of the icon */
+  justify-self: end; /* Aligns the icon to the end of the grid column */
+}
 </style>
