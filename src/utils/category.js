133a7f45910a7350634929bbdff144305aff5932
Vemund Ellingsson Røe <32141244+vemunderoe@users.noreply.github.com>
2024-04-05 10:31:15 +0200

    feat: Get labels for enums to display in dropdowns

--- src/utils/category.js
+++ src/utils/category.js
@@ -10,8 +10,27 @@ const Category = {
 function getCategories() {
   return Object.keys(Category).map(key => ({
     value: Category[key],
-    label: key.charAt(0) + key.slice(1).toLowerCase(),
+    label: getCategoryLabel(Category[key]),
   }));
+}
+
+function getCategoryLabel(type) {
+  switch (type) {
+    case Category.FOOD:
+      return "Food";
+    case Category.DRINK:
+      return "Drink";
+    case Category.CLOTHING:
+      return "Clothing";
+    case Category.ELECTRONICS:
+      return "Electronics";
+    case Category.BOOKS:
+      return "Books";
+    case Category.GENERAL:
+      return "General";      
+    default:
+      return "Unknown";
+  }
 }
 
 export { Category, getCategories };
