b2b4f73b1a30c20a6f704e122272f2ee5a49cb88
birtheec <Birthe.e.christiansen@gmail.com>
2024-04-06 09:51:43 +0200

    style: QuizView question count

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -50,9 +50,10 @@ const formattedDate = computed(() => {
           <p><strong>Last edited: </strong>{{ formattedDate }} </p>
           <p><strong>Description: </strong>{{MainStore.state.quiz.quiz.description}}</p>
           <p><strong>Category: </strong>{{MainStore.state.quiz.quiz.category}}</p>
+          <p class="questions-count">{{ MainStore.state.quiz.quiz.questions.length }} <strong>Questions</strong></p>
         </div>
         <div class="quizView-statistics">
-          <h3><Strong>Recent attempts</Strong></h3>
+          <h3><Strong>Recent attempts:</Strong></h3>
           <ul>
             <li v-for="(scores, index) in MainStore.state.quiz.quiz.scores" :key="index">
               <p class="scores-text">{{ "Score: " + scores.score }}/{{ scores.total }} - {{ scores.date }}</p>
