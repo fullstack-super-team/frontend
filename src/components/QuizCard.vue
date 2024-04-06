03821a2439ef17b8c7b9f840fb3f8a39f64493ff
birtheec <Birthe.e.christiansen@gmail.com>
2024-04-06 11:27:01 +0200

    Style: QuizCard

--- src/components/QuizCard.vue
+++ src/components/QuizCard.vue
@@ -1,10 +1,30 @@
 <script setup>
+/**
+ * A Vue component script for displaying a quiz with a randomly selected color theme.
+ * The component expects a `quiz` object as a prop, which should contain details
+ * about the quiz such as id, title, category, questions, and the number of questions.
+ */
+
+// Imports from vue-router and vue for routing and reactive references.
 import { RouterLink } from 'vue-router';
 import { ref } from 'vue';
 
+/**
+ * An array of colors used for the quiz cards.
+ * @type {string[]}
+ */
 const colors = ['#ff914d', '#1792ea', '#78d64f', '#8c52ff']
+
+/**
+ * A reactive reference to a randomly chosen color from the `colors` array.
+ * @type {Ref<string>}
+ */
 const randomColor = ref(colors[Math.floor(Math.random() * colors.length)]);
 
+/**
+ * Defines the props expected by the component.
+ * The `quiz` prop is required and has a default structure if not provided.
+ */
 defineProps({
   quiz: {
     type: Object,
@@ -39,6 +59,7 @@ defineProps({
   margin: 10px 0;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   transition: transform 0.2s ease-in-out;
+  max-width: 33%;
 }
 
 .quiz-card:hover {
@@ -50,6 +71,10 @@ h3 {
   margin: 0;
   color: white;
   font-size: 24px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  box-sizing: border-box;
 }
 
 .category {
@@ -67,6 +92,7 @@ h3 {
   .quiz-card {
     padding: 10px;
     flex: 80%;
+    max-width: 100%;
   }
 
   h3 {
