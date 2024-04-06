0c5f0b06704b83602b40689825bb06859e54f055
Vemund Ellingsson Røe <32141244+vemunderoe@users.noreply.github.com>
2024-04-06 10:05:45 +0200

    fix: Add correct identifiers to correctly update form

--- src/components/QuizForm.vue
+++ src/components/QuizForm.vue
@@ -15,6 +15,10 @@ const props = defineProps({
     type: String,
     required: true,
   },
+  submitFormText: {
+    type: String,
+    default: 'Save',
+  },
   initialValues: {
     type: Object,
     default: () => ({}),
@@ -26,45 +30,45 @@ const props = defineProps({
 })
 
 const defaultValues = {
+  identifier: `qDate.now()`,
   title: "",
   description: "",
   category: Category.GENERAL,
   difficultyLevel: DifficultyLevel.EASY,
-  questions: [
-    {
-      identifier: Date.now(),
-      text: "",
-      points: 100,
-      type: QuestionType.TEXT,
-      answers: [
-        {              
-          text: "",
-          isCorrect: false,
-        }
-      ],
-    }
-  ],
+  questions: [],
 };
 
 const mergedValues = { ...defaultValues, ...props.initialValues };
 const quiz = reactive(mergedValues);
 
-for (const question of quiz.questions) {
-  console.log(question)
+let questionIdentifier = 0;
+for (const question of quiz.questions) {  
+  let answerIdentifier = 0;
+  question.identifier = `q${questionIdentifier}`;
+  if (question.type === QuestionType.TEXT || question.type === QuestionType.TRUE_OR_FALSE) {
+    for (const answer of question.answers) {
+      answer.identifier = `${question.identifier}-a${answerIdentifier}`;
+      answerIdentifier++;
+    }    
+  } else if (question.type === QuestionType.SLIDE) {
+    question.answer.identifier = `${question.identifier}-a${answerIdentifier}`;
+    answerIdentifier++;
+  }  
+  questionIdentifier++;  
 }
-console.log(quiz)
 
 const formSubmitted = ref(false);
 
 const addQuestion = () => {
+  const identifier = `q${Date.now()}`;  
   quiz.questions.push({
-    identifier: Date.now(),
+    identifier: identifier,
     text: "",
     points: 100,
     type: QuestionType.TEXT,
     answers: [
       {
-        identifier: Date.now(),
+        identifier: `${identifier}-a${Date.now()}`,
         text: '',
         isCorrect: false,
       }
@@ -72,13 +76,13 @@ const addQuestion = () => {
   });
 };
 
-function updateQuestion(updatedQuestion, index) {    
+function updateQuestion(updatedQuestion, identifier) {    
+  const index = quiz.questions.findIndex((question) => question.identifier === identifier);  
   quiz.questions[index] = updatedQuestion;
 }
 
-const deleteQuestion = (questionIdentifier) => {
-  const index = quiz.questions.findIndex((question) => question.identifier === questionIdentifier);
-  if (index === -1) return;
+const deleteQuestion = (identifier) => {  
+  const index = quiz.questions.findIndex((question) => question.identifier === identifier);
   quiz.questions.splice(index, 1);
 };
 
@@ -91,22 +95,22 @@ function emitSubmit() {
   <form novalidate @submit.prevent="emitSubmit">
     <div class="upperBar">  
       <h1>{{ formTitle }}</h1>      
-      <Button type="submit">Save</Button>
+      <Button type="submit">{{ submitFormText }}</Button>
     </div>
-    <Input label="Quiz title: *" placeholder="Enter your quiz title here" v-model="quiz.title"
+    <Input id="quizTitle" label="Quiz title: *" placeholder="Enter your quiz title here" v-model="quiz.title"
       :class="{ 'is-invalid': formSubmitted && !quiz.title }" />
     <p v-if="formSubmitted && !quiz.title" class="validation-error">Quiz title is required.</p>    
 
     <div class="dropdownMenus">
       <h2>Select a Category</h2>
-      <select v-model="quiz.category">
+      <select id="quizCategory" v-model="quiz.category">
         <option v-for="category in getCategories()" :key="category.value" :value="category.value">
           {{ category.label }}
         </option>
       </select>
 
       <h2>Select a difficulty</h2>
-      <select v-model="quiz.difficultyLevel">
+      <select id="quizDifficultyLevel"v-model="quiz.difficultyLevel">
         <option v-for="difficultyLevel in getDifficultyLevels()" :key="difficultyLevel.value"
           :value="difficultyLevel.value">
           {{ difficultyLevel.label }}
@@ -114,13 +118,16 @@ function emitSubmit() {
       </select>
     </div>
 
-    <TextArea v-model="quiz.description" label="Description" placeholder="Enter quiz description here..."
+    <TextArea id="quizDescription" v-model="quiz.description" label="Description" placeholder="Enter quiz description here..."
       :charLimit="200" />
 
-    <h2>Questions: {{ quiz.questions.length }}</h2>
-    <Button @click="addQuestion">Add Question</Button>
-    <CreateQuestion v-for="(question, index) in quiz.questions" :key="question.identifier" :question="question"
-      @update-question="updateQuestion($event, index)" @delete-question="deleteQuestion(question.identifier)" />
+    <h2>Questions</h2>
+    <CreateQuestion v-for="(question) in quiz.questions" :key="question.identifier" :identifier="question.identifier" :question="question"
+      @update-question="updateQuestion($event, question.identifier)" @delete-question="deleteQuestion(question.identifier)" />
+    <div class="lowerBar">
+      <Button @click="addQuestion">Add Question</Button>
+      <Button type="submit" v-if="quiz.questions.length > 0">{{ submitFormText }}</Button>
+    </div>
   </form>
 </template>
 
@@ -129,6 +136,27 @@ function emitSubmit() {
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+
+.lowerBar {
+  display: flex;
+  justify-content: space-between;
+}
+
+.is-invalid {
+  border-color: red;
+  /* Add your error styling here */
+}
+
+.validation-error {
+  color: red;
+  /* Style for the validation message */
+  font-size: 0.9em;
+}
+
+.dropdownMenus {
+  margin-bottom: 20px;
+  /* Additional style for spacing */
 }
 
 </style>
