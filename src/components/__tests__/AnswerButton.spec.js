fd7df4abee61d4d13fd0d2dcb1842f131fd62fdf
Lotte Christine Walla Aune <lcaune@stud.ntnu.no>
2024-04-08 00:39:50 +0200

    test: added AnswerButton test

--- src/components/__tests__/AnswerButton.spec.js
+++ src/components/__tests__/AnswerButton.spec.js
@@ -15,9 +15,9 @@ describe('AnswerButton', () => {
         expect(wrapper.text()).toBe('test');
     });
 
-    /**
 
-    it('renders the correct icon when the answer is selected and correct', () => {
+
+    it('renders the correct icon when the answer is selected and correct', async () => {
         const wrapper = mount(AnswerButton, {
             props: {
                 answerText: 'test',
@@ -26,10 +26,55 @@ describe('AnswerButton', () => {
                 isTrueOrFalseQuestion: false
             }
         });
-        const correctIcon = wrapper.find('img[src="@/assets/correct.png"]');
-        expect(correctIcon.exists()).toBe(true);
-    });
-        // */
+        const correctIcon = wrapper.find('img');
 
+        expect(correctIcon.exists()).toBe(true);
+        expect(correctIcon.attributes('src')).toBe('/src/assets/correct.png');
+    });
+
+    it('renders the correct icon when the answer is selected and incorrect', async () => {
+        const wrapper = mount(AnswerButton, {
+            props: {
+                answerText: 'test',
+                isCorrect: false,
+                isAnswerSelected: true,
+                isTrueOrFalseQuestion: false
+            }
+        });
+        const correctIcon = wrapper.find('img');
+
+        expect(correctIcon.exists()).toBe(true);
+        expect(correctIcon.attributes('src')).toBe('/src/assets/wrong.png');
+    });
+
+    it('renders the correct icon when the answer text is "True", answer is not selected and the question is a true or false question', async () => {
+        const wrapper = mount(AnswerButton, {
+            props: {
+                answerText: 'True',
+                isCorrect: true,
+                isAnswerSelected: false,
+                isTrueOrFalseQuestion: true
+            }
+        });
+        const correctIcon = wrapper.find('img');
+
+        expect(correctIcon.exists()).toBe(true);
+        expect(correctIcon.attributes('src')).toBe('/src/assets/thumbsUp.png');
+    });
+
+    it('renders the correct icon when the answer text is "False", answer is not selected and the question is a true or false question', async () => {
+        const wrapper = mount(AnswerButton, {
+            props: {
+                answerText: 'False',
+                isCorrect: false,
+                isAnswerSelected: false,
+                isTrueOrFalseQuestion: true
+            }
+        });
+        const correctIcon = wrapper.find('img');
+
+        expect(correctIcon.exists()).toBe(true);
+        expect(correctIcon.attributes('src')).toBe('/src/assets/thumbsDown.png');
+    });
 
 });
