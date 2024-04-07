e0c9591bdeeadbc2dd8abd0c67dc4d72232467e5
Lotte Christine Walla Aune <lcaune@stud.ntnu.no>
2024-04-08 00:24:55 +0200

    test: added test to AnswerButton

--- src/components/__tests__/AnswerButton.spec.js
+++ src/components/__tests__/AnswerButton.spec.js
@@ -3,14 +3,33 @@ import { mount } from '@vue/test-utils';
 import AnswerButton from "@/components/AnswerButton.vue";
 
 describe('AnswerButton', () => {
-    it('Plz run', () => {
+    it('renders AnswerButton properly', () => {
         const wrapper = mount(AnswerButton, {
             props: {
-                answer: 'test',
-                correctAnswer: 'test',
-                isAnswered: false
+                answerText: 'test',
+                isCorrect: true,
+                isAnswerSelected: false,
+                isTrueOrFalseQuestion: false
             }
         });
         expect(wrapper.text()).toBe('test');
     });
+
+    /**
+
+    it('renders the correct icon when the answer is selected and correct', () => {
+        const wrapper = mount(AnswerButton, {
+            props: {
+                answerText: 'test',
+                isCorrect: true,
+                isAnswerSelected: true,
+                isTrueOrFalseQuestion: false
+            }
+        });
+        const correctIcon = wrapper.find('img[src="@/assets/correct.png"]');
+        expect(correctIcon.exists()).toBe(true);
+    });
+        // */
+
+
 });
